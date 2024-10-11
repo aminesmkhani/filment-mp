ef4bed21477bdb90133f3b8454583475f31d8cdf
Aмιη <amin.esmkhani75@gmail.com>
2024-10-11 09:50:50 +0330

    add enums class and refactor same!

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
