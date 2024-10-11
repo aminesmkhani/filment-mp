fb93480c6f104c4cc85466d371b46b57f2ebc298
Aмιη <amin.esmkhani75@gmail.com>
2024-10-11 09:38:52 +0330

    Speaker Model Refactor and add Taks Relations!

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -11,10 +11,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Speaker extends Model
 {
     use HasFactory;
+
+    const QUALIFICATIONS = [
+        'business-leader' => 'Business Leader',
+        'charisma' => 'Charismatic Speaker',
+        'first-time' => 'First Time Speaker',
+        'hometown-hero' => 'Hometown Hero',
+        'humanitarian' => 'Works in Humanitarian Field',
+        'laracast-contributor' => 'Laracast Contributor',
+        'twitter-influencer' => 'Large Twitter Following',
+        'youtube-influencer' => 'Large Youtube Following',
+        'open-source' => 'Open Source Creator / Maintainer',
+        'unique-perspective' => 'Unique perspective',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -38,6 +52,11 @@ class Speaker extends Model
         'id' => 'integer',
         'qualifications' => 'array'
     ];
+
+    public function talks(): HasMany
+    {
+        return $this->hasMany(Talk::class);
+    }
 
     public function conferences(): BelongsToMany
     {
@@ -65,18 +84,7 @@ class Speaker extends Model
              ->columnSpanFull()
              ->searchable()
              ->bulkToggleable()
-             ->options([
-                 'business-leader' => 'Business Leader',
-                 'charisma' => 'Charismatic Speaker',
-                 'first-time' => 'First Time Speaker',
-                 'hometown-hero' => 'Hometown Hero',
-                 'humanitarian' => 'Works in Humanitarian Field',
-                 'laracast-contributor' => 'Laracast Contributor',
-                 'twitter-influencer' => 'Large Twitter Following',
-                 'youtube-influencer' => 'Large Youtube Following',
-                 'open-source' => 'Open Source Creator / Maintainer',
-                 'unique-perspective' => 'Unique perspective',
-             ])
+             ->options(self::QUALIFICATIONS)
             ->descriptions([
                 'business-leader' => 'Large Business Leader for World Wide Company!',
                 'charisma' => 'Charismatic Speaker',
