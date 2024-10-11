6893e52517ff4e563c0477584d837671f691d9ab
Aмιη <amin.esmkhani75@gmail.com>
2024-10-11 09:34:32 +0330

    add file upload media library!

--- app/Filament/Resources/VenueResource.php
+++ app/Filament/Resources/VenueResource.php
@@ -18,7 +18,7 @@ class VenueResource extends Resource
 {
     protected static ?string $model = Venue::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-globe-asia-australia';
 
     public static function form(Form $form): Form
     {
